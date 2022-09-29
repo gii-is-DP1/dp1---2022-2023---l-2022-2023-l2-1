11f27e5ab5293ee7caf65f15a9017bc8b376d443
davgavser <davgavser@alum.us.es>
2022-09-29 14:21:49 +0200

    Davgavser añadido como owner

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -11,6 +11,9 @@ INSERT INTO authorities(id,username,authority) VALUES (3,'vet1','veterinarian');
 INSERT INTO users(username,password,enabled) VALUES ('inmmaysal','1nm4',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (4,'inmmaysal','owner');
 
+INSERT INTO users(username,password,enabled) VALUES ('davgavser','1234',TRUE);
+INSERT INTO authorities(id,username,authority) VALUES (5,'davgavser','owner');
+
 
 
 INSERT INTO vets(id, first_name,last_name) VALUES (1, 'James', 'Carter');
@@ -49,7 +52,7 @@ INSERT INTO owners VALUES (8, 'Maria', 'Escobito', '345 Maple St.', 'Madison', '
 INSERT INTO owners VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', '6085559435', 'owner1');
 INSERT INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487', 'owner1');
 INSERT INTO owners VALUES (11, 'Inma', 'Mayo', 'Reina Mercedes', 'Sevilla', '625845678', 'inmmaysal');
-
+INSERT INTO owners VALUES (12, 'David', 'Gavira', 'Reina Mercedes', 'Sevilla', '722446559', 'davgavser');
 
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (1, 'Leo', '2010-09-07', 1, 1);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (2, 'Basil', '2012-08-06', 6, 2);
@@ -65,6 +68,7 @@ INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (11, 'Freddy', '201
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (12, 'Lucky', '2010-06-24', 2, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (13, 'Sly', '2012-06-08', 1, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (14, 'Juanita', '2022-09-29', 7, 11);
+INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (15, 'Tina', '2008-08-12', 2, 12);
 
 
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (1, 7, '2013-01-01', 'rabies shot');
