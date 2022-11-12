e0baccf51ac4c826e29656d0a66366e55a7efd43
aleotpag <a.ortiz.pagador@gmail.com>
2022-11-12 21:17:19 +0100

    Entidad historico

---
+++ src/main/java/org/springframework/samples/petclinic/historico/Historico.java
@@ -0,0 +1,60 @@
+package org.springframework.samples.petclinic.historico;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "historicos")
+public class Historico extends BaseEntity {
+
+    @NotNull
+    @JoinColumn(name = "user_id")
+    private Integer userId;
+
+    @NotNull
+    @Column(name = "partidas_totales")
+    private Integer partidasTotales;
+
+    @NotNull
+    @Column(name = "partidas_ganadas")
+    private Integer partidasGanadas;
+
+    // @NotNull
+    // @Column(name = "historial_partidas")
+    // private List<Partida> historialPartidas;
+
+    @NotNull
+    @Column(name = "minas_encontradas")
+    private Integer minasEncontrads;
+
+    @NotNull
+    @Column(name = "puntuacion")
+    private Integer puntuacion;
+
+    @NotNull
+    @Column(name = "tiempo_total_juego")
+    private Double tiempoTotalJuego;
+
+    @NotNull
+    @Column(name = "tiempo_medio_partida")
+    private Double tiempoMedioPartida;
+
+    @NotNull
+    @Column(name = "tiempo_minimo")
+    private Double tiempoMinimo;
+
+    @NotNull
+    @Column(name = "tiempo_maximo")
+    private Double tiempoMaximo;
+
+}
