cf338fee07ddfdacb495c5550650aa2a86afaaa5
JUAN CARLOS LOPEZ VEIGA <juanlopezveiga@gmail.com>
2022-11-13 18:11:30 +0100

    validation  v2

--- src/main/java/org/springframework/samples/petclinic/user/User.java
+++ src/main/java/org/springframework/samples/petclinic/user/User.java
@@ -7,10 +7,26 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+
+// @PasswordValueMatch.List({
+// 		@PasswordValueMatch(	
+// 				field = "password",
+// 				fieldMatch = "confirmPassword",
+// 				message = "Passwords do not match!"
+// 			)
+// })
+	
+@AllArgsConstructor
+@NoArgsConstructor	
 @Getter
 @Setter
 @Entity
@@ -19,9 +35,11 @@ public class User{
 	@Id
 	String username;
 	
-	
+	@ValidPassword
+	@NotBlank(message = "New password is mandatory")
+	@NotEmpty
 	String password;
-	
+
 	boolean enabled;
 	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
