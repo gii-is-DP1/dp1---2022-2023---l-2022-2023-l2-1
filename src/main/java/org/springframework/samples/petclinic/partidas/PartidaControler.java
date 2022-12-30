6a952055d3c128170ed6575678b8f8c5ca6855f9
JuanluRM <juanluis.ruano.muriedas@gmail.com>
2022-12-30 14:28:56 +0100

    save

--- src/main/java/org/springframework/samples/petclinic/partidas/PartidaControler.java
+++ src/main/java/org/springframework/samples/petclinic/partidas/PartidaControler.java
@@ -6,8 +6,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.registeredUser.RegisteredUser;
 import org.springframework.samples.petclinic.registeredUser.RegisteredUserService;
