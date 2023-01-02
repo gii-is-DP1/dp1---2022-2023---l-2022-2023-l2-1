30f7fd9869cda30610e5ad46715f955705678e9c
aleotpag <a.ortiz.pagador@gmail.com>
2023-01-02 16:37:29 +0100

    Tablero terminado, pendiente de revision

--- src/main/resources/static/js/algTablero.js
+++ src/main/resources/static/js/algTablero.js
@@ -1,58 +1,268 @@
-
 const tablero = {
-  numMinasTotales : 30,
-  numMinasEncontradas : 0,
+  numMinasTotales: 1,
+  numMinasEncontradas: 0,
   numBanderas: 0,
-  filas: document.getElementById("numFilas").value,
-  columnas: document.getElementById("numColumnas").value,
-  aCampoMinas:[]
-}
+  numFilas: 4, //document.getElementById("numFilas").value,
+  numColumnas: 4, //document.getElementById("numColumnas").value,
+  campoMinas: [],
+};
 
-buscaminas();
+window.onload = buscaminas();
 
-function buscaminas(){
+function buscaminas() {
   pintaCabecera();
   pintaTablero();
   creaCampoMinasVacio();
+  esparcirMinas();
+  contarMinas();
 }
 
-function pintaCabecera(){
-  num_mina.innerHTML = tablero.numMinasTotales;
+// --------------------------------------------------------
+
+function pintaCabecera() {
   num_bandera.innerHTML = tablero.numBanderas;
-}    
+  num_mina.innerHTML = tablero.numMinasTotales - tablero.numMinasEncontradas;
+}
 
 function pintaTablero() {
-  var divTablero = document.getElementById("tablero");
-  for (var f = 0; f < tablero.filas; f++) {
+  let divTablero = document.getElementById("tablero");
+  for (let f = 0; f < tablero.numFilas; f++) {
     // Filas
-    var divFila = document.createElement("div");
+    let divFila = document.createElement("div");
     divFila.setAttribute("id", "fila");
     divTablero.appendChild(divFila);
-  for (var c = 0; c < tablero.columnas; c++) {
-    // Casillas
-    var divCasilla = document.createElement("div");
-    divCasilla.setAttribute("id", "casilla");
-    divCasilla.setAttribute("fila", f);
-    divCasilla.setAttribute("columna", c);
-    //Eventos de casilla
-    divCasilla.addEventListener("contextmenu", marcar);
-    divCasilla.addEventListener("click", destapar);
-    divFila.appendChild(divCasilla);
-  }
+    for (let c = 0; c < tablero.numColumnas; c++) {
+      // Casillas
+      let divCasilla = document.createElement("div");
+      divCasilla.setAttribute("id", "f" + f + "_c" + c);
+      divCasilla.dataset.fila = f;
+      divCasilla.dataset.columna = c;
+      //Eventos de casilla
+      divCasilla.addEventListener("contextmenu", accionMarcar);
+      divCasilla.addEventListener("click", accionDestapar);
+      divFila.appendChild(divCasilla);
+    }
   }
 }
 
-function marcar(evento){
-  tablero.numBanderas = tablero.numBanderas + 1;
+// --------------------------------------------------------
+
+function accionMarcar(evento) {
+  if (evento.type === "contextmenu") {
+    console.log(evento);
+
+    let casilla = evento.currentTarget;
+    evento.stopPropagation();
+    evento.preventDefault();
+
+    let fila = parseInt(casilla.dataset.fila);
+    let columna = parseInt(casilla.dataset.columna);
+    if (
+      fila >= 0 &&
+      columna >= 0 &&
+      fila < tablero.numFilas &&
+      columna < tablero.numColumnas
+    ) {
+      if (casilla.classList.contains("icon-bandera")) {
+        casilla.classList.remove("icon-bandera");
+        tablero.numBanderas--;
+      } else if (casilla.classList.length == 0) {
+        casilla.classList.add("icon-bandera");
+        tablero.numBanderas++;
+      }
+    }
+  }
   pintaCabecera();
 }
 
-function destapar(){
-  tablero.numMinasTotales = tablero.numMinasTotales - 1;
-  tablero.numMinasEncontradas = tablero.numMinasEncontradas + 1;
-  pintaCabecera();  
+function accionDestapar(evento) {
+  if (evento.type === "click") {
+    let casilla = evento.currentTarget;
+    let fila = parseInt(casilla.dataset.fila, 10);
+    let columna = parseInt(casilla.dataset.columna, 10);
+    destaparCasilla(fila, columna);
+  }
+  pintaCabecera();
 }
 
-function creaCampoMinasVacio(){
-  tablero.aCampoMinas = new Array(tablero.filas);
+function destaparCasilla(fila, columna) {
+  console.log(
+    "Detapamos casilla con Fila: " + fila + ", y Columna: " + columna
+  );
+  if (
+    fila > -1 &&
+    fila < tablero.numFilas &&
+    columna > -1 &&
+    columna < tablero.numColumnas
+  ) {
+    let casilla = document.querySelector("#f" + fila + "_c" + columna);
+    if (!casilla.classList.contains("destapada")) {
+      if (!casilla.classList.contains("icon-bandera")) {
+        casilla.classList.add("destapada");
+        casilla.innerHTML = tablero.campoMinas[fila][columna];
+        casilla.classList.add("c" + tablero.campoMinas[fila][columna]);
+        if (tablero.campoMinas[fila][columna] != "mina") {
+          if (tablero.campoMinas[fila][columna] == 0) {
+            destaparCasilla(fila - 1, columna - 1);
+            destaparCasilla(fila - 1, columna);
+            destaparCasilla(fila - 1, columna + 1);
+            destaparCasilla(fila, columna - 1);
+            destaparCasilla(fila, columna + 1);
+            destaparCasilla(fila + 1, columna - 1);
+            destaparCasilla(fila + 1, columna);
+            destaparCasilla(fila + 1, columna + 1);
+            casilla.innerHTML = "";
+          }
+          finDePartida("");
+        } else if (tablero.campoMinas[fila][columna] == "mina") {
+          casilla.innerHTML = "";
+          casilla.classList.add("icon-mina");
+          casilla.classList.add("sinmarcar");
+          finDePartida("error");
+        }
+      }
+    }
+  }
+}
+
+// --------------------------------------------------------
+
+function creaCampoMinasVacio() {
+  tablero.campoMinas = new Array(tablero.numFilas);
+  for (let fila = 0; fila < tablero.numFilas; fila++) {
+    tablero.campoMinas[fila] = new Array(tablero.numColumnas);
+  }
+}
+
+function esparcirMinas() {
+  let numMinasEsparcidas = 0;
+  while (numMinasEsparcidas < tablero.numMinasTotales) {
+    let fila = Math.floor(Math.random() * tablero.numFilas);
+    let columna = Math.floor(Math.random() * tablero.numColumnas);
+    if (tablero.campoMinas[fila][columna] != "mina") {
+      tablero.campoMinas[fila][columna] = "mina";
+      numMinasEsparcidas++;
+    }
+  }
+}
+
+// --------------------------------------------------------
+
+function contarMinasAlrededorCasilla(fila, columna) {
+  let numMinas = 0;
+  for (let zFila = fila - 1; zFila <= fila + 1; zFila++) {
+    for (let zColumna = columna - 1; zColumna <= columna + 1; zColumna++) {
+      if (
+        zFila > -1 &&
+        zFila < tablero.numFilas &&
+        zColumna > -1 &&
+        zColumna < tablero.numColumnas
+      ) {
+        if (tablero.campoMinas[zFila][zColumna] == "mina") {
+          numMinas++;
+        }
+      }
+    }
+  }
+  tablero.campoMinas[fila][columna] = numMinas;
+}
+
+function contarMinas() {
+  for (let fila = 0; fila < tablero.numFilas; fila++) {
+    for (columna = 0; columna < tablero.numColumnas; columna++) {
+      if (tablero.campoMinas[fila][columna] != "mina") {
+        contarMinasAlrededorCasilla(fila, columna);
+      }
+    }
+  }
+}
+
+// --------------------------------------------------------
+
+function listaCasillas() {
+  let casillas = [];
+  let filas = document.getElementById("tablero").children;
+  for (let zfila = 1; zfila < filas.length; zfila++) {
+    for (
+      let zcasilla = 0;
+      zcasilla < filas[zfila].children.length;
+      zcasilla++
+    ) {
+      casillas.push(filas[zfila].children[zcasilla]);
+    }
+  }
+  return casillas;
+}
+
+function todasDestapadas() {
+  let res = true;
+  let casillas = listaCasillas();
+  for (let i = 0; i < casillas.length; i++) {
+    let casilla = casillas[i];
+    if (
+      !casilla.classList.contains("destapada") &&
+      !casilla.classList.contains("icon-bandera")
+    ) {
+      res = false;
+    }
+  }
+  return res;
+}
+
+function finDePartida(msg) {
+  let s = "Fin de partida: ";
+  if (msg == "error") {
+    resolverTablero();
+    alert(s + "Mala suerte!!!");
+  } else {
+    if (todasDestapadas()) {
+      let estado = resolverTablero();
+      if ((estado == "banderas_erroneas")) {
+        alert(s + "Hay banderas incorrectas");
+      } else if (estado == "minas_sin_destapar") {
+        alert(s + "Hay minas sin destapar");
+      } else if (estado == "todas_banderas_correctas") {
+        alert(s + "Enhorabuena!!!");
+      }
+    }
+  }
+}
+
+function resolverTablero() {
+  let casillas = listaCasillas();
+  let bandera_erronea = 0;
+  let bandera_correcta = 0;
+  let mina_sin_destapar = 0;
+  for (let i = 0; i < casillas.length; i++) {
+    casillas[i].removeEventListener("click", accionDestapar);
+    casillas[i].removeEventListener("contextmenu", accionMarcar);
+
+    let fila = parseInt(casillas[i].dataset.fila, 10);
+    let columna = parseInt(casillas[i].dataset.columna, 10);
+
+    if (casillas[i].classList.contains("icon-bandera")) {
+      casillas[i].classList.remove("icon-bandera"); 
+      casillas[i].classList.add("destapada");
+      if (tablero.campoMinas[fila][columna] == "mina") {
+        casillas[i].classList.add("bandera-correcta");
+        bandera_correcta++;
+      } else {
+        casillas[i].classList.add("bandera-erronea");
+        bandera_erronea++;
+      }
+    } else if (!casillas[i].classList.contains("destapada")) {
+      casillas[i].classList.add("destapada");
+      if (tablero.campoMinas[fila][columna] == "mina") {
+        casillas[i].classList.add("icon-mina");
+        mina_sin_destapar++;
+      }
+    }
+  }
+  if (bandera_erronea > 0) {
+    return "banderas_erroneas";
+  } else if (mina_sin_destapar > 0) {
+    return "minas_sin_destapar";
+  } else if (bandera_correcta == tablero.numMinasTotales) {
+    return "todas_banderas_correctas";
+  }
 }
--- src/main/webapp/WEB-INF/jsp/tablero/tablero.jsp
+++ src/main/webapp/WEB-INF/jsp/tablero/tablero.jsp
@@ -5,8 +5,7 @@ prefix="petclinic" tagdir="/WEB-INF/tags" %> <%@ taglib prefix="c"
 uri="http://java.sun.com/jsp/jstl/core" %>
 <!-- %@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %-->
 <style>
-
-  #cabecera_tablero{
+  #cabecera_tablero {
     padding: 10px;
     width: 99%;
     margin: auto;
@@ -15,30 +14,32 @@ uri="http://java.sun.com/jsp/jstl/core" %>
     margin-bottom: 10px;
   }
 
-  #img_bandera, #img_mina{
+  #img_bandera,
+  #img_mina {
     width: 30px;
     height: 30px;
   }
 
-  #num_bandera, #num_mina{
+  #num_bandera,
+  #num_mina {
     font-size: 25px;
     color: rgb(8, 3, 83);
   }
 
-  #img_bandera{
+  #img_bandera {
     float: left;
   }
 
-  #num_bandera{
+  #num_bandera {
     float: left;
     margin-left: 10px;
   }
 
-  #img_mina{
+  #img_mina {
     float: right;
   }
 
-  #num_mina{
+  #num_mina {
     float: right;
     margin-right: 10px;
   }
@@ -55,69 +56,97 @@ uri="http://java.sun.com/jsp/jstl/core" %>
   #fila {
     display: flex;
   }
-  #casilla {
-    border: 1px solid black;
+  #tablero div div {
+    border: 2px solid black;
     width: 40px;
     height: 40px;
     margin: 2px;
     background-color: rgb(130, 130, 228);
+    font-size: 20px;
+    text-align: center;
+    line-height: 45px;
   }
-  h1{
+  h1 {
     text-align: center;
   }
 
+  /*  */
+
+  .icon-bandera:before {
+    content: "\2691";
+    color: rgb(187, 0, 0);
+    font-size: 30px;
+  }
+
+  .icon-mina:before {
+    content: "\2699";
+    color: black;
+    font-size: 30px;
+  }
+
+  .bandera-erronea:before {
+    content: "\2612";
+    color: red;
+    font-size: 40px;
+  }
+  .bandera-correcta:before {
+    content: "\2611";
+    color: green;
+    font-size: 40px;
+  }
+
+  #tablero div div.destapada {
+    border-style: none;
+  }
+
+  #tablero div div.c1 {
+    color: aqua;
+  }
+  #tablero div div.c2 {
+    color: blue;
+  }
+  #tablero div div.c3 {
+    color: green;
+  }
+  #tablero div div.c4 {
+    color: greenyellow;
+  }
+  #tablero div div.c5 {
+    color: steelblue;
+  }
+  #tablero div div.c6 {
+    color: brown;
+  }
+  #tablero div div.c7 {
+    color: blueviolet;
+  }
+  #tablero div div.c8 {
+    color: darkgoldenrod;
+  }
 </style>
 
+<head>
+  <c:url value="/resources/images/bandera.png" var="img_bandera" />
+  <c:url value="/resources/images/minesweeper.png" var="img_mina" />
+  <c:url value="/js/algTablero.js" var="algTablero" />
+  <c:url value="/css/style.css" var="estilo" />
+  <!-- <link href="${estilo}" rel="stylecheet" type="text/css" /> -->
+</head>
+
 <petclinic:layout pageName="tablero">
-  <c:url value="/resources/images/bandera.png" var="img_bandera"/>
-  <c:url value="/resources/images/minesweeper.png" var="img_mina"/>
-  <c:url value="/js/algTablero.js" var="algTablero"/>
-  
   <!-- Datos del tablero para el js -->
-  <input type="hidden" id="numFilas" value="${tablero.filas}"/>
-  <input type="hidden" id="numColumnas" value="${tablero.columnas}"/>
+  <input type="hidden" id="numFilas" value="${tablero.filas}" />
+  <input type="hidden" id="numColumnas" value="${tablero.columnas}" />
   <!--  -->
 
-  <h1>Tablero JS</h1>
   <div id="tablero">
     <div id="cabecera_tablero">
-    <img id="img_bandera" src="${img_bandera}"/>
-    <span id="num_bandera"></span>
-    <img id="img_mina" src="${img_mina}"/>
-    <span id="num_mina"></span>
-</div>
-</div>
-
-<br>
-<br>
-
-<h1>Tablero inicial</h1>
-    <div
-      class="tablero"
-      style="
-        border: 2px solid black;
-        margin: auto;
-        width: max-content;
-        padding: 10px;
-      "
-    >
-      <c:forEach begin="1" step="1" end="${tablero.filas}">
-        <div class="fila" style="display: flex; margin: 1px">
-          <c:forEach begin="1" step="1" end="${tablero.columnas}">
-            <div
-              class="casilla"
-              style="
-                border: 1px solid black;
-                width: 40px;
-                height: 40px;
-                margin: 2px;
-                background-color: rgb(130, 130, 228);
-              "
-            ></div>
-          </c:forEach>
-        </div>
-      </c:forEach>
+      <img id="img_bandera" src="${img_bandera}" />
+      <span id="num_bandera"></span>
+      <img id="img_mina" src="${img_mina}" />
+      <span id="num_mina"></span>
     </div>
+  </div>
 
-<script type="text/javascript" src="${algTablero}"></script>
+  <script type="text/javascript" src="${algTablero}"></script>
 </petclinic:layout>
