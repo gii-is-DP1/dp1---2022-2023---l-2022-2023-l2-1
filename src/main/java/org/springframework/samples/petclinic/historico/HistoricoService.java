6e41687e7f79e64983862d79fa2b8f6293abab9b
JuanluRM <juanluis.ruano.muriedas@gmail.com>
2023-01-11 18:37:17 +0100

    cambios

--- src/main/java/org/springframework/samples/petclinic/historico/HistoricoService.java
+++ src/main/java/org/springframework/samples/petclinic/historico/HistoricoService.java
@@ -1,8 +1,6 @@
 package org.springframework.samples.petclinic.historico;
 
 import java.util.List;
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
