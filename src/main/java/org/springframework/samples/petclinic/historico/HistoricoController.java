819744b08e74613a9f289d9cab4b3fb23f07fb9a
davidg43 <80384087+davidg43@users.noreply.github.com>
2023-01-11 16:56:49 +0100

    Solucion error

--- src/main/java/org/springframework/samples/petclinic/historico/HistoricoController.java
+++ src/main/java/org/springframework/samples/petclinic/historico/HistoricoController.java
@@ -1,29 +1,18 @@
 package org.springframework.samples.petclinic.historico;
 
 
-import java.lang.ProcessBuilder.Redirect;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-
-import javax.security.auth.message.config.AuthConfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.registeredUser.RegisteredUser;
 import org.springframework.samples.petclinic.registeredUser.RegisteredUserService;
-import org.springframework.samples.petclinic.user.Authorities;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.samples.petclinic.partidas.PartidaService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
